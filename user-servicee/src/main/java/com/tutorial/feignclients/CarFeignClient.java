bf68729c7af0895625b08a4583508e2863f35c04
Ricardo Tovar <ricardotovar.grupobruild@gmail.com>
2022-04-21 16:51:54 -0500

    eureka 2

--- user-servicee/src/main/java/com/tutorial/feignclients/CarFeignClient.java
+++ user-servicee/src/main/java/com/tutorial/feignclients/CarFeignClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.tutorial.model.Car;
 
 
-@FeignClient(name="car-service",url = "http://localhost:8002",path = "/car")
+@FeignClient(name="car-service",path = "/car")
 public interface CarFeignClient {
 
 
